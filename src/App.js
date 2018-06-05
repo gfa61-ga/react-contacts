7166b321bf361b2aef92a73dc0b30059f25ebd6f
George Al <gfa61.ga@gmail.com>
2018-06-06 00:23:52 +0300

    feat: Lesson 3.20 The Search Field - A Controlled Component

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       "email": "tyler@reacttraining.com",
       "avatarURL": "http://localhost:5001/tyler.jpg"
     }
-  ], inputValue: 5}
+  ]}
 
   removeContact = function(contact) { // if we write mevoveContact as arrow function
                                       // we dont' have to pass 'this' to ListContacts Component
@@ -31,16 +31,10 @@ class App extends Component {
     ))
   }
 
-  hundleInput = (event) => {
-
-    this.setState({inputValue: (event.target.value)/2})
-  }
-
   render() {
     return (
       <div>
         <ListContacts parent={this} contacts={this.state.contacts} OnRemoveContact={this.removeContact}/>
-        <form><input onChange={this.hundleInput} value={this.state.inputValue}/></form>
       </div>
     )
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,37 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 
-function ListContacts(props) {
-    return (
-        <ol className='contact-list'>
-            {props.contacts.map(contact =>
-                <li className='contact-list-item' key={contact.id}>
-                    <img className='contact-avatar' alt={"Avatar Image of " + contact.name} src={contact.avatarURL}/>
-                {/* Teachers solution:
-                    <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}/>
-                */}
-                    <div className='contact-details'>
-                        <p>{contact.name}</p>
-                        <p>{contact.email}</p>
-                    </div>
-                    <button onClick={function() {props.OnRemoveContact.call(props.parent, contact)}} className='contact-remove'/>
-{/* if App.removeContact is written as arrow function we don't have to use call() :
-                    <button onClick={function() {props.OnRemoveContact(contact)}} className='contact-remove'/>
+class ListContacts extends Component {
 
-                    // or even better:
-                    <button onClick={() => props.OnRemoveContact(contact)} className='contact-remove'/>
-*/}
-                </li>
-            )}
-        </ol>
-    )
-}
+    static propTypes = {
+      parent: PropTypes.object.isRequired,
+      contacts: PropTypes.array.isRequired,
+      OnRemoveContact: PropTypes.func.isRequired
+    }
 
-ListContacts.propTypes = {
-  parent: PropTypes.object.isRequired,
-  contacts: PropTypes.array.isRequired,
-  OnRemoveContact: PropTypes.func.isRequired
+    state = {query: ''}
+
+    searchQuery = (event) => {
+        this.setState({query: event.target.value})
+    }
+
+    render() {
+        return (
+            <div className='list-contacts'>
+                <form className='list-contacts-top'>
+                    <input className='search-contacts' type='text'
+                        placeholder='Search contacts'
+                        value={this.state.query}
+                        onChange={this.searchQuery}
+                    />
+                </form>
+                {JSON.stringify(this.state)}
+                <ol className='contact-list'>
+                    {this.props.contacts.map(contact =>
+                        <li className='contact-list-item' key={contact.id}>
+                            <img className='contact-avatar' alt={"Avatar Image of " + contact.name} src={contact.avatarURL}/>
+                        {/* Teachers solution:
+                            <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}/>
+                        */}
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            {/* if we don't use arrow functin, 'this' will refer to button element: */}
+                            <button onClick={() => {this.props.OnRemoveContact.call(this.props.parent, contact)}} className='contact-remove'/>
+        {/* if App.removeContact is written as arrow function we don't have to use call() :
+                            <button onClick={function() {props.OnRemoveContact(contact)}} className='contact-remove'/>
+
+                            // or even better:
+                            <button onClick={() => props.OnRemoveContact(contact)} className='contact-remove'/>
+        */}
+                        </li>
+                    )}
+                </ol>
+            </div>
+        )
+    }
 }
 
 export default ListContacts;
