68ebb54e5bfacb4786c6a384807e3f34e713fad4
George Al <gfa61.ga@gmail.com>
2018-06-07 17:45:14 +0300

    feat: Lesson 4 - Render UI with External data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'; //import ListContacts from './ListContacts.js';
 
+import * as ContactsAPI from './utils/ContactsAPI';
+
+// My error code for import:
+// import ContactsAPI from './utils/ContactsAPI';
+
 class App extends Component {
-  state = {contacts: [
-    {
-      "id": "ryan",
-      "name": "Ryan Florence",
-      "email": "ryan@reacttraining.com",
-      "avatarURL": "http://localhost:5001/ryan.jpg"
-    },
-    {
-      "id": "michael",
-      "name": "Michael Jackson",
-      "email": "michael@reacttraining.com",
-      "avatarURL": "http://localhost:5001/michael.jpg"
-    },
-    {
-      "id": "tyler",
-      "name": "Tyler McGinnis",
-      "email": "tyler@reacttraining.com",
-      "avatarURL": "http://localhost:5001/tyler.jpg"
-    }
-  ]}
+  state = {contacts: []}
+
+  componentDidMount() {
+    // My solution:
+ // ContactsAPI.getAll().then((data) => this.setState({contacts : data}))
+    // Teacher's solution:
+    ContactsAPI.getAll().then((contacts) => this.setState({contacts}))
+    // Which is equivalent to:
+ // ContactsAPI.getAll().then((contacts) => this.setState({contacts : contacts}))
+  }
 
   removeContact = function(contact) { // if we write mevoveContact as arrow function
                                       // we dont' have to pass 'this' to ListContacts Component
@@ -29,6 +24,8 @@ class App extends Component {
       {contacts: prevState.contacts.filter(c => c.id !== contact.id)
       }
     ))
+
+    ContactsAPI.remove(contact);
   }
 
   render() {
