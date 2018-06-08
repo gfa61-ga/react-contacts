7865908dfcecab2947ee72f5efcce9232d696a23
George Al <gfa61.ga@gmail.com>
2018-06-08 18:05:17 +0300

    feat: Lesson 5.6 - Finishing The Contact Form

--- src/App.js
+++ src/App.js
@@ -35,11 +35,34 @@ class App extends Component {
 
 
   addContact = (contact) => {
+
+    // My first solution:
+    /*
     contact.id = contact.name.split(" ").join("-").toLowerCase();
     this.setState((prevState) => {
         prevState.contacts.push(contact);
         return {contacts: prevState.contacts}
     })
+    */
+
+    // My solution.. there was an error..!!
+    // ContactsAPI.create(contact).then(() => ContactsAPI.getAll).then((contacts) => this.setState({contacts}))
+
+    // My second solution:
+    /*
+    ContactsAPI.create(contact).then((contact) =>  // create() adds a contact to the database and returns it
+    this.setState((prevState) => {
+        prevState.contacts.push(contact);
+        return {contacts: prevState.contacts}
+    }))
+    */
+
+    // Teacher's solution
+    ContactsAPI.create(contact).then((contact) =>
+      this.setState((prevState) => (
+        {contacts: prevState.contacts.concat(contact)} // {contacts: prevState.contacts.concat([contact])}
+      )
+    ))
   }
 
 
@@ -59,11 +82,25 @@ class App extends Component {
             OnRemoveContact={this.removeContact}
           />
           )}/>
-        <Route path='/create' render={() => (
+        <Route path='/create' render={({ history }) => ( // render() gets {history} value from route object..!!
           <CreateContact
-            OnAddContact={this.addContact}
+            OnAddContact={(contact) => {/*My solution
+                                        * OnAddContact={this.addContact}
+                                        */
+              this.addContact(contact)
+              history.push('/') // this changes the url to '/'
+              // console.log(history)
+            }}
           />
         )}/>
+
+        <Route path='/' render={({history}) => { return (
+          <div>
+            {JSON.stringify(history)}
+          </div>
+          )
+          }
+        }/>
       </div>
     )
   }
