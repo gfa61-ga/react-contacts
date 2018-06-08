23859511ebb0653eaea2bd34e97590f9ea3e3bd5
George Al <gfa61.ga@gmail.com>
2018-06-08 13:20:47 +0300

    feat: Lesson 5.6  Build Out The Contact Form

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import ImageInput from './ImageInput'
+import { Link } from 'react-router-dom'
 
 class CreateContact extends Component {
     render() { return (
-        <div className='create-contact-form'>
-            Add new contact
+        <div>
+            <Link className='close-create-contact' to='/'/>
+            <form className='create-contact-form'>
+                <ImageInput
+                    className='create-contact-avatar-input'
+                    name='avatarUrl'
+                    maxHeight={64}
+                />
+                <div className='create-contact-details'>
+
+                    {/* My solution:
+                        <p><label>First Name</label> <input type='text'/></p>
+                        <p><label>Last Name</label> <input type='text'/></p>
+                    */}
+                    <input type='text' name='name' placeholder='Name'/>
+                    <input type='text' name='email' placeholder='Email'/>
+
+                    <p><button>Add Contact</button></p>
+                </div>
+            </form>
         </div>)
     }
 }
